c768ec9b42ebc1c2a4f085a9f142bab21f034cb8
Mohamed Ali <ma8510007@gmail.com>
2025-05-08 05:58:44 +0300

    Implement API service and models for login functionality

--- lib/core/routing/routes.dart
+++ lib/core/routing/routes.dart
@@ -1,5 +1,4 @@
 class Routes {
-  static const String splash = '/splash';
   static const String onboarding = '/onboarding';
   static const String login = '/login';
   static const String home = '/home';
